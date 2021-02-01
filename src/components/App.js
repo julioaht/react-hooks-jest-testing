512eeb70a139e7e74f0352fe7cb1c75031c87617
Julio Hernandez <julioandres72@gmail.com>
2021-02-01 14:45:17 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Article from "./Article";
+import Header from "./Header";
 
 function App() {
-  return <div className="App">learn react!</div>;
+  return (
+    <div className="App">
+     <Header />
+     <Article /> 
+      
+    </div>
+  )
 }
 
 export default App;
